05e5738cd65051b84442ffc1028cee0d29fa6757
mangonob <mangonob@qq.com>
2024-07-29 02:07:37 +0800

    feat: iterator

--- hello.lua
+++ hello.lua
@@ -1,5 +1,20 @@
 require("./utils")
 
-for line in io.lines("./hello.lua") do
-    print(line)
+function foo(...)
+    for i = 1, 10 do
+        coroutine.yield(i * i)
+    end
 end
+
+co = coroutine.create(foo)
+
+while true do
+    local success, v = coroutine.resume(co)
+    if coroutine.status(co) ~= "dead" then
+        print(v)
+    else
+        break
+    end
+end
+
+print("Iterator finish")
